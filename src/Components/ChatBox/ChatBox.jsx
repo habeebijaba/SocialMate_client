1ccbbb3322eb8391a920051def68aba731561314
Habeeb <habeebav841@gmail.com>
2023-04-06 02:46:46 +0530

    bug fixing

--- src/Components/ChatBox/ChatBox.jsx
+++ src/Components/ChatBox/ChatBox.jsx
@@ -15,7 +15,7 @@ import Message from "../Message/Message";
 import io from 'socket.io-client';
 
 
-const socket = io.connect("wss://heavenslice.ml/api");
+const socket = io.connect("wss://heavenslice.ml/");
 
 const ChatBox = () => {
 
